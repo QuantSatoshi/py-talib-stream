d17fc41e9731b6a31315d4cec47228f91cfbdfc6
Andres Suazo <andresfsuazo@gmail.com>
2020-12-16 20:17:35 -0600

    modified: atr_keeper.py

--- pytalibstream/atr_keeper.py
+++ pytalibstream/atr_keeper.py
@@ -1,13 +1,10 @@
-from typing import Optional
 import pyslidingwindow
-from pytalibstream.types.hlc import HLC
 
 
 class AtrKeeper:
-    def __init__(self, max_len: Optional[int] = 10):
+    def __init__(self, max_len: int):
         if max_len < 2:
-            raise Exception(
-                "AtrKeeper period must be >= 2")
+            raise Exception("AtrKeeper period must be >= 2")
 
         self.high = pyslidingwindow.SlidingWindowFloat(max_len)
         self.low = pyslidingwindow.SlidingWindowFloat(max_len)
@@ -17,26 +14,25 @@ class AtrKeeper:
         self.max_len = max_len
         self.prev_tr = []
 
-    def add(self, candle: HLC):
+    def add(self, high: float, low: float, close: float):
         self.data_count += 1
-        self.high.push(candle.high)
-        self.low.push(candle.low)
-        self.close.push(candle.close)
+        self.high.push(high)
+        self.low.push(low)
+        self.close.push(close)
 
         if self.max_len + 1 > self.data_count > 1:
-            self.prev_tr.append(self.tr)
-        elif self.data_count == self.max_len+1:
+            self.prev_tr.append(self.calc_tr())
+        elif self.data_count == self.max_len + 1:
             self._atr = sum(self.prev_tr) / len(self.prev_tr)
-            self._atr = (self._atr * (self.max_len - 1) + self.tr) / self.max_len
+            self._atr = (self._atr * (self.max_len - 1) + self.calc_tr()) / self.max_len
         else:
-            self._atr = (self._atr * (self.max_len - 1) + self.tr) / self.max_len
+            self._atr = (self._atr * (self.max_len - 1) + self.calc_tr()) / self.max_len
 
     @staticmethod
-    def get_tr(high: float, low: float, close: float):
-        return max(high - low, abs(high - close), abs(low - close))
+    def get_tr(high: float, low: float, prev_close: float):
+        return max(high - low, abs(high - prev_close), abs(low - prev_close))
 
-    @property
-    def tr(self):
+    def calc_tr(self):
         close = self.close.get(-2) if self.close.length() > 1 else 0
         return self.get_tr(self.high.last(), self.low.last(), close)
 
--- pytalibstream/tests/test_atr_keeper.py
+++ pytalibstream/tests/test_atr_keeper.py
@@ -1,19 +1,27 @@
 import unittest
-
+import numpy as np
+import talib
 from pytalibstream.atr_keeper import AtrKeeper
-from pytalibstream.types.hlc import HLC
 
 
 class TestAtrKeeper(unittest.TestCase):
-
     def setUp(self) -> None:
         self.atr_keeper = AtrKeeper(5)
-        self.sample = range(1, 21)
+        self.sample = np.random.uniform(1, 2, (30, 3))
+        self.sample[:, 0] = self.sample[:, 0] + 1
+        self.sample[:, 1] = self.sample[:, 1] - 1
 
     def test_add(self):
-        for i in self.sample:
-            candle = HLC.from_list([i + 1, i, i * .90])
-            self.atr_keeper.add(candle)
+        atr_ta = talib.ATR(
+            high=self.sample[:, 0], low=self.sample[:, 1], close=self.sample[:, 2], timeperiod=5
+        )
+        for i in range(0, len(self.sample)):
+            self.atr_keeper.add(self.sample[i, :][0], self.sample[i, :][1], self.sample[i, :][2])
+
+            if i > 20:
+                self.assertTrue(
+                    abs(self.atr_keeper.atr - atr_ta[i]) < 0.0001
+                )
 
     def test_get_tr(self):
         actual = AtrKeeper.get_tr(3, 1, 2)
